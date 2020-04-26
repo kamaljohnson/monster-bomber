cafae87bb8f83434faf0d9a42e3d1b39a3cfd27c
Kamal Johnson <kamaljohnson12345@gmail.com>
2020-04-26 08:51:13 +0530

    :sparkles: Add player movement, :sparkles: Add NavMesh Agents/obstacles

---
+++ Crowd Bomber Mania/Assets/Scripts/PersonMovementController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Serialization;
+
+public class PersonMovementController : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Vector2 groundDimention;
+    
+    private Vector3 _destination;
+    private bool _destinationReached;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _destination = transform.position;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (CheckDestinationReached())
+        {
+            Move();
+        }
+    }
+
+    private void Move()
+    {
+        var z = groundDimention.y;
+        var x = groundDimention.x;
+        var y = transform.position.y;
+
+        _destinationReached = false;
+        
+        _destination = new Vector3(Random.Range(-x, x), y, Random.Range(-z, z));
+        agent.SetDestination(_destination);
+    }
+
+    private bool CheckDestinationReached()
+    {
+        const float tolerance = 0.5f;
+        return Mathf.Abs(Vector3.Distance(transform.position, _destination)) <= tolerance;
+    }
+}
