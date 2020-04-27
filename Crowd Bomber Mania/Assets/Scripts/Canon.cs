4b5c26f607eecc4e9f1b239bfc355a52c7c67bf7
Kamal Johnson <kamaljohnson12345@gmail.com>
2020-04-27 09:56:09 +0530

    :sparkles: Add cannon shoot to location

--- Crowd Bomber Mania/Assets/Scripts/Canon.cs
+++ Crowd Bomber Mania/Assets/Scripts/Canon.cs
@@ -5,12 +5,59 @@ using UnityEngine.Serialization;
 
 public class Canon : MonoBehaviour
 {
-
     public GameObject cannonBall;
+
+    public Transform shootTransform;
     
-    // shoots the cannon ball to this position
-    public void Shoot(Vector2 position)
+    private Rigidbody cannonballInstance;
+
+    [SerializeField]
+    [Range(10f, 80f)]
+    private float angle = 45f;
+
+    private void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                FireCannonAtPoint(hitInfo.point);
+            }
+        }
+    }
+
+    private void FireCannonAtPoint(Vector3 point)
+    {
+        var rotation = transform.rotation;
+        transform.LookAt(point);
+        rotation.eulerAngles = new Vector3(rotation.x,transform.rotation.eulerAngles.y , rotation.z);
+        transform.rotation = rotation;
+
+        var cannonBallObject = GameObject.Instantiate(cannonBall);
+        cannonballInstance = cannonBallObject.GetComponent<Rigidbody>();
         
+        var velocity = BallisticVelocity(point, angle);
+        Debug.Log("Firing at " + point + " velocity " + velocity);
+        
+        cannonballInstance.transform.position = shootTransform.position;
+        cannonballInstance.velocity = velocity;
+    }
+
+    private Vector3 BallisticVelocity(Vector3 destination, float angle)
+    {
+        Vector3 dir = destination - shootTransform.position; // get Target Direction
+        float height = dir.y; // get height difference
+        dir.y = 0; // retain only the horizontal difference
+        float dist = dir.magnitude; // get horizontal direction
+        float a = angle * Mathf.Deg2Rad; // Convert angle to radians
+        dir.y = dist * Mathf.Tan(a); // set dir to the elevation angle.
+        dist += height / Mathf.Tan(a); // Correction for small height differences
+
+        // Calculate the velocity magnitude
+        float velocity = Mathf.Sqrt(dist * Physics.gravity.magnitude / Mathf.Sin(2 * a));
+        return velocity * dir.normalized; // Return a normalized vector.
     }
 }
